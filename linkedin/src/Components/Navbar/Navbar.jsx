478d60b08e7b3bde24df7c7b896acbcd802937e9
tminaidis9 <sdi1900113@di.uoa.gr>
2024-04-19 15:21:13 +0300

    240419:1521

--- linkedin/src/Components/Navbar/Navbar.jsx
+++ linkedin/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 import './Navbar.css'
-import logo from '../../assets/MyLinkLogo.png'
+import logo from '../../assets/Logo.png'
+import info from '../../assets/information.png'
+
+
 
 const Navbar = () => {
   return (
     <nav>
         <img src={logo} alt=""/>
         <ul>
-            <li>Sign In</li>
-            <li>Sing up</li>
+            <li class="signIn">Sign In</li>
+            <li class="signUp">Sing Up</li>
+            <li class="sendHelp"><img src={info} alt=""/> Send Help!</li>
         </ul>
     </nav>
   )
