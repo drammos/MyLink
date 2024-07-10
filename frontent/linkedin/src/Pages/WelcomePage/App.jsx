5fe40ee7f5bda56c57b1244f7900bc2cf4a3d022
tminaidis9 <theomin2001@gmail.com>
2024-07-11 01:53:42 +0300

    240711 - Continue editing Descrption 1,2

--- frontent/linkedin/src/Pages/WelcomePage/App.jsx
+++ frontent/linkedin/src/Pages/WelcomePage/App.jsx
@@ -3,15 +3,19 @@ import Navbar from "../../Components/Navbar/Navbar"
 import WelcomePageLogIn from "../../Components/WelcomePageLogIn/WelcomePageLogIn"
 import Footer from "../../Components/Footer/Footer"
 import Description1 from "../../Components/Description/Description1"
+import Description2 from "../../Components/Description/Description2"
 import './App.css'
 
 const App = () => {
   return (
     <div>
         <Navbar/>
-        <div class="WelcomePageMain">
-            <WelcomePageLogIn/>
-            <Description1/>
+          <div className="WelcomePageMain">
+              <div className="row">
+                  <WelcomePageLogIn/>
+                  <Description1/>
+              </div>
+            <Description2/>
         </div>
         <Footer/>
     </div>
