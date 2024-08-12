0c94dd438ef3fdcfc67d42714e0c98751ade1fe7
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-08-12 17:13:29 +0300

    Update Program.cs

--- WebAppBackEnd/WebApplicationMyLink/Program.cs
+++ WebAppBackEnd/WebApplicationMyLink/Program.cs
@@ -8,7 +8,7 @@ using MyLink.Data.Repository;
 using MyLink.Data.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
-
+const string policyName = "CorsPolicy";
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -30,7 +30,15 @@ builder.Services.AddIdentityCore<User>(options =>
     })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName, builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,10 +49,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.UseCors(opt =>
-{
-    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:5175");
-});
+app.UseCors(policyName);
 app.UseAuthentication();
 app.UseAuthorization();
 
