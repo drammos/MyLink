c1ec7497efceb4dae1f8c4ccdca1efb89eb431a9
drammos <jrammos@outlook.com.gr>
2024-08-24 22:00:02 +0300

    Remove the command for delete user table

--- WebAppBackEnd/MyLink.Data/Initialize/InitializerDb.cs
+++ WebAppBackEnd/MyLink.Data/Initialize/InitializerDb.cs
@@ -26,6 +26,48 @@ namespace MyLink.Data.Initialize
                 var result = await users.CreateAsync(user, "1234@Password");
                 await users.AddToRoleAsync(user, "Admin");
 
+                User user1 = new User()
+                {
+                    FirstName = "Dimitris",
+                    LastName = "Rammos",
+                    PhoneNumber = "1234567890",
+                    UserName = "drammos",
+                    Email = "drammos@outlook.com.gr",
+                    PictureURL = "rammos.com"
+                };
+
+                result = await users.CreateAsync(user1, "1234@Password");
+                await users.AddToRoleAsync(user1, "Professional");
+
+                User user2 = new User()
+                {
+                    FirstName = "Dimitris",
+                    LastName = "Rammos",
+                    PhoneNumber = "1234567890",
+                    UserName = "frammos",
+                    Email = "frammos@outlook.com.gr",
+                    PictureURL = "rammos.com"
+                };
+
+                result = await users.CreateAsync(user2, "1234@Password");
+                await users.AddToRoleAsync(user2, "Professional");
+
+                User user3 = new User()
+                {
+                    FirstName = "Dimitris",
+                    LastName = "Rammos",
+                    PhoneNumber = "1234567890",
+                    UserName = "prammos",
+                    Email = "prammos@outlook.com.gr",
+                    PictureURL = "rammos.com"
+                };
+
+                result = await users.CreateAsync(user3, "1234@Password");
+                await users.AddToRoleAsync(user3, "Professional");
+
+
+
+
                 await db.SaveChangesAsync();
             }
         }
