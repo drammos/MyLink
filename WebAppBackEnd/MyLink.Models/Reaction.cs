f926e62fd8dbd1eb5e8a89379ec8b86a2c6e093c
drammos <jrammos@outlook.com.gr>
2024-08-27 15:59:11 +0300

    Add Models for Post,Reaction and comment

---
+++ WebAppBackEnd/MyLink.Models/Reaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLink.Models
+{
+    public class Reaction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ReactionType { get; set; }
+
+        [Required]
+        [ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public Post Post { get; set; } = null!;
+
+        [Required]
+        [ForeignKey("UserId")]
+        public string? UserId { get; set; } 
+        public User User { get; set; } = null!;
+    }
+}
--- WebAppBackEnd/MyLink.Models/User.cs
+++ WebAppBackEnd/MyLink.Models/User.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.Collections;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyLink.Models
@@ -14,7 +13,10 @@ namespace MyLink.Models
         public string? PictureURL { get; set; }
 
         public ICollection<Education> Educations { get; } = new List<Education>();
-        public ICollection<Experience> Experiences { get; } = new List<Experience>();      
+        public ICollection<Experience> Experiences { get; } = new List<Experience>();
+        public ICollection<Post> Posts { get; } = new List<Post>();
+        public ICollection<Comment> MyComments { get; set; } = new List<Comment>();
+        public ICollection<Reaction> MyReactions { get; set; } = new List<Reaction>();
         public ICollection<User> ConnectedUsers { get; set; } = [];
         public ICollection<User> PendingRequestUsers { get; set; } = [];
         public ICollection<User> InComingRequestUsers { get; set; } = [];
