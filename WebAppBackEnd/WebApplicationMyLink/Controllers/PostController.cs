477c5bdcb140940da711db0082c0b98413fe6e82
drammos <jrammos@outlook.com.gr>
2024-08-27 15:59:33 +0300

    Add PostController

---
+++ WebAppBackEnd/WebApplicationMyLink/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLink.Data.Repository.IRepository;
+
+namespace WebAppMyLink.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PostController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
