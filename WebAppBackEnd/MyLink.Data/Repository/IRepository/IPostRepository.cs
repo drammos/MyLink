b29102fa09a459d6ff0c4d4afd7f998cff0e228b
drammos <jrammos@outlook.com.gr>
2024-08-30 18:05:48 +0300

    Add fuctionality for comments and reactions

--- WebAppBackEnd/MyLink.Data/Repository/IRepository/IPostRepository.cs
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IPostRepository.cs
@@ -7,5 +7,12 @@ namespace MyLink.Data.Repository.IRepository
     {
         public Post Update(UpdatePostDTO updatePostDTO);
         public bool AddComment(Comment comment);
+        public bool DeleteComment(int postId, int commentId);
+        public Task<List<Comment>> GetComments(int postId);
+        public Task<List<Comment>> GetUserComments(string userId);
+        public bool AddReaction(Reaction reaction);
+        public bool DeleteReaction(int postId, int reactionId);
+        public Task<List<Reaction>> GetReactions(int postId);
+        public Task<List<Reaction>> GetUserReactions(string userId);
     }
 }
--- WebAppBackEnd/MyLink.Data/Repository/PostRepository.cs
+++ WebAppBackEnd/MyLink.Data/Repository/PostRepository.cs
@@ -2,6 +2,7 @@
 using MyLink.Models;
 using MyLink.Data.Access;
 using MyLink.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyLink.Data.Repository
 {
@@ -41,6 +42,78 @@ namespace MyLink.Data.Repository
 
             post.Comments.Add(comment);
             return true;
+        }
+
+        public bool DeleteComment(int postId, int commentId)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.Id == postId);
+            if (post == null) return false;
+
+            var comment = post.Comments.FirstOrDefault(c => c.Id == commentId);
+            if (comment == null) return false;
+
+            post.Comments.Remove(comment);
+            return true;
+        }
+
+        public async Task<List<Comment>> GetComments(int postId)
+        {
+            var comments = await _context.Posts
+                .Include(x => x.Comments)
+                .FirstOrDefaultAsync(x => x.Id == postId);
+
+            if (comments == null) return null;
+            return [.. comments!.Comments];
+        }
+
+        public async Task<List<Comment>> GetUserComments(string userId)
+        {
+            var comments = await _context.Posts
+                .Include(x => x.Comments)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+            
+            if (comments == null) return null;
+            return [.. comments!.Comments];
+        }
+
+        public bool AddReaction(Reaction reaction)
+        {
+            var id = reaction.PostId;
+            var post = _context.Posts.FirstOrDefault(p => p.Id == id);
+            if (post == null) return false;
+
+            post.Reactions.Add(reaction);
+            return true;
+        }
+        public bool DeleteReaction(int postId, int reactionId)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.Id == postId);
+            if (post == null) return false;
+
+            var reaction = post.Reactions.FirstOrDefault(c => c.Id == reactionId);
+            if (reaction == null) return false;
+
+            post.Reactions.Remove(reaction);
+            return true;
+        }
+        public async Task<List<Reaction>> GetReactions(int postId)
+        {
+            var reactions = await _context.Posts
+                .Include(x => x.Reactions)
+                .FirstOrDefaultAsync(x => x.Id == postId);
+
+            if (reactions == null) return null;
+            return [.. reactions!.Reactions];
+        }
+
+        public async Task<List<Reaction>> GetUserReactions(string userId)
+        {
+            var reactions = await _context.Posts
+                .Include(x => x.Reactions)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (reactions == null) return null;
+            return [.. reactions!.Reactions];
         }
     }
 }
