c1c9fca11e9dc5f712a88a72b61cfe4fca3778fc
drammos <jrammos@outlook.com.gr>
2024-09-05 15:34:43 +0300

    Add job Repository

---
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IJobRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyLink.Models;
+
+namespace MyLink.Data.Repository.IRepository
+{
+    public interface IJobRepository : IRepositoryBase<Job>
+    {
+        public Job Update(UpdateJobDTO updateJobDTO);
+    }
+}
---
+++ WebAppBackEnd/MyLink.Data/Repository/JobRepository.cs
@@ -0,0 +1,35 @@
+﻿using MyLink.Data.Access;
+using MyLink.Models.DTOS;
+using MyLink.Models;
+
+namespace MyLink.Data.Repository
+{
+    public class JobRepository : RepositoryBase<Post>, IJobRepository
+    {
+        private ApplicationDbContext _context;
+
+        public JobRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Post Update(UpdateJobDTO updateJobDTO)
+        {
+            var id = updateJobDTO.Id;
+            var post = _context.Jobs.FirstOrDefault(u => u.Id == id);
+            if (post == null)
+            {
+                return null;
+            }
+
+            post.Title = updatePostDTO.Title;
+            post.UpdateAt = updatePostDTO.UpdateAt;
+            post.VideoUrls = updatePostDTO.VideoUrls;
+            post.PictureUrls = updatePostDTO.PictureUrls;
+            post.Content = updatePostDTO.Content;
+            post.IsLikedByCurrentUser = updatePostDTO.IsLikedByCurrentUser;
+
+            return post;
+        }
+    }
+}
