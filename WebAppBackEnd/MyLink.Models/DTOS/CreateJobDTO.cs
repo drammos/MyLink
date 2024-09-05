4f73202066a5c75253200501e6860699280a22d2
drammos <jrammos@outlook.com.gr>
2024-09-05 15:35:13 +0300

    Create DTOs for job

---
+++ WebAppBackEnd/MyLink.Models/DTOS/CreateJobDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLink.Models.DTOS
+{
+    public class CreateJobDTO
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string WorkType { get; set; }
+        [Required]
+        public string LocationType { get; set; }
+        [Required]
+        public DateTime PostedDate { get; set; }
+    }
+}
---
+++ WebAppBackEnd/MyLink.Models/DTOS/UpdateJobDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLink.Models.DTOS
+{
+    public class UpdateJobDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string WorkType { get; set; }
+        [Required]
+        public string LocationType { get; set; }
+    }
+}
