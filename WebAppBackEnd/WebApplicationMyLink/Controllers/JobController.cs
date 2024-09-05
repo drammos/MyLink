c0d106d96b32eee01974f5c145640c81a43689fb
drammos <jrammos@outlook.com.gr>
2024-09-05 15:36:13 +0300

    Create Job Controller

---
+++ WebAppBackEnd/WebApplicationMyLink/Controllers/JobController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLink.Data.Repository.IRepository;
+using MyLink.Models.DTOS;
+using MyLink.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAppMyLink.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+
+        public JobController(IUnitOfWork unitOfWork, UserManager<User> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        [HttpPost("CreateJob")]
+        public async Task<ActionResult<Job>> CreateJob([FromForm] CreateJobDTO createJobDTO)
+        {
+            User user = await _userManager.FindByIdAsync(createJobDTO.UserId);
+            if (user == null) return NotFound();
+
+            Job job = new Job()
+            {
+                Title = createJobDTO.Title,
+                CompanyName = createJobDTO.CompanyName,
+                Description = createJobDTO.Description,
+                Location = createJobDTO.Location,
+                WorkType = createJobDTO.WorkType,
+                LocationType = createJobDTO.LocationType,
+                PostedDate = createJobDTO.PostedDate,
+                UserId = createJobDTO.UserId
+            };
+
+            _unitOfWork.Job
+        }
+    }
+}
