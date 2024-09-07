165b5dcc5f241347007cd9327f78dea92c79b19e
Δημητρης Ραμμος <dimitrisrammos@Demetress-Mac-mini.local>
2024-09-08 01:13:40 +0300

    Add new functions in JobRepository for endpoints

--- WebAppBackEnd/MyLink.Data/Repository/IRepository/IJobRepository.cs
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IJobRepository.cs
@@ -8,6 +8,10 @@ namespace MyLink.Data.Repository.IRepository
         public Job Update(UpdateJobDTO updateJobDTO);
         public Task<List<JobApplication>> GetUserAppliedJobs(string username);
         public Task<List<JobApplication>> GetUserStatusJobs(string username, JobApplicationStatus status = JobApplicationStatus.Pending);
+        public bool ApplyForJob(JobApplication jobApplication);
         public Task<bool> AcceptedJobApplication(int jobApplicationId);
+        public Task<bool> RejectJobApplication(int jobApplicationId);
+        public Task<bool> WithdrawnJobApplication(int jobApplicationId);
+        public Task<bool> DeleteAllJobApplications(int jobApplicationId);
     }
 }
--- WebAppBackEnd/MyLink.Data/Repository/JobRepository.cs
+++ WebAppBackEnd/MyLink.Data/Repository/JobRepository.cs
@@ -27,10 +27,11 @@ namespace MyLink.Data.Repository
             job.WorkType = updateJobDTO.WorkType;
             job.Location = updateJobDTO.Location;
             job.LocationType = updateJobDTO.LocationType;
+            job.Category = updateJobDTO.Category;
 
             return job;
         }
-        
+
         public async Task<List<JobApplication>> GetUserAppliedJobs(string username)
         {
             return await _context.Jobs
@@ -47,11 +48,20 @@ namespace MyLink.Data.Repository
                 .ToListAsync();
         }
 
+        public bool ApplyForJob(JobApplication jobApplication)
+        {
+            var jobId = jobApplication.Id;
+            var job = _context.Jobs.FirstOrDefault(p => p.Id == jobId);
+            if (job == null) return false;
+
+            job.JobApplications.Add(jobApplication);
+            return true;
+        }
+
         public async Task<bool> AcceptedJobApplication(int jobApplicationId)
         {
             var jobApplication = await _context.JobApplications
                 .FindAsync(jobApplicationId);
-
             if (jobApplication == null) return false;
 
             jobApplication.Status = JobApplicationStatus.Accepted;
@@ -59,5 +69,42 @@ namespace MyLink.Data.Repository
 
             return true;
         }
+
+        public async Task<bool> RejectJobApplication(int jobApplicationId)
+        {
+            var jobApplication = await _context.JobApplications
+                .FindAsync(jobApplicationId);
+            if (jobApplication == null) return false;
+
+            jobApplication.Status = JobApplicationStatus.Rejected;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        
+        public async Task<bool> WithdrawnJobApplication(int jobApplicationId)
+        {
+            var jobApplication = await _context.JobApplications
+                .FindAsync(jobApplicationId);
+            if (jobApplication == null) return false;
+
+            jobApplication.Status = JobApplicationStatus.Withdrawn;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteAllJobApplications(int jobId)
+        {
+            var job = await _context.Jobs
+                .Include(j => j.JobApplications)
+                .FirstOrDefaultAsync(j => j.Id == jobId);
+            if(job == null) return false;
+            
+            job.JobApplications.Clear();
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
     }
 }
