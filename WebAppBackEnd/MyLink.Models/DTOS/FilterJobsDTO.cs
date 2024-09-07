6e9feaadc0e7e7db21d99603cf5ea0e2cfa33ffe
Δημητρης Ραμμος <dimitrisrammos@Demetress-Mac-mini.local>
2024-09-08 01:43:49 +0300

    Add FildetJobsDto for searching

---
+++ WebAppBackEnd/MyLink.Models/DTOS/FilterJobsDTO.cs
@@ -0,0 +1,11 @@
+
+namespace MyLink.Models.DTOS
+{
+    public class FilterJobsDTO
+    {
+        public int PostedDays { get; set; }
+        public string LocationType { get; set; }
+        public string WorkType { get; set; }
+        public string Category { get; set; }
+    }
+}
