a7b0915f6c08d29ab47db0a3d6c1a4be5a5ac67a
drammos <jrammos@outlook.com.gr>
2024-09-12 15:47:44 +0300

    Create Meatada.cs

---
+++ WebAppBackEnd/MyLink.Models/Pagination/Metadata.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyLink.Models.Pagination
+{
+    public class Metadata
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
