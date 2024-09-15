95709a78b40d3e51c814a5886cc6a72b032006c2
tminaidis9 <theomin2001@gmail.com>
2024-09-16 00:54:26 +0300

    fix post section on main page without functionality

--- frontent/linkedin/src/Components/AfterLoginComponents/MainPageElements/MainPageCenter.jsx
+++ frontent/linkedin/src/Components/AfterLoginComponents/MainPageElements/MainPageCenter.jsx
@@ -1,68 +1,69 @@
-import React from "react";
-import './styles/MainPageCenter.css'
+﻿import React, { useState } from "react";
+import { FaEdit, FaFileAlt, FaVideo, FaCamera, FaUserCircle, FaThumbsUp, FaGlobe } from "react-icons/fa";
+import './styles/MainPageCenter.css';
 
 const MainPageCenter = () => {
+    const [postContent, setPostContent] = useState("");
+
+    const handlePostSubmit = () => {
+        // Handle post submission logic here (e.g., send to API)
+        console.log("Post Submitted:", postContent);
+        setPostContent("");  // Clear the input after submission
+    };
+
     return (
         <div className="main-page-center">
+            {/* New Post Section */}
             <div className="new-post-container">
                 <div className="new-post">
-                    <button type="button" className="new-post-btn">
-                        <i className="fa fa-edit"></i>&nbsp;Start a post
-                    </button>
-                    <button type="button" className="share-media">
-                        <i className="fa fa-file-alt"></i>
-                    </button>
-                    <button type="button" className="share-media">
-                        <i className="fa fa-video"></i>
-                    </button>
-                    <button type="button" className="share-media">
-                        <i className="fa fa-camera"></i>
-                    </button>
+                    <textarea
+                        className="post-textarea"
+                        placeholder="What's on your mind?"
+                        value={postContent}
+                        onChange={(e) => setPostContent(e.target.value)}
+                    />
                 </div>
-                <div className="article">
-                    <a href="#" className="article-link">Write an article</a>&nbsp;on LinkedIn
-                </div>
-            </div>
-
-            <div className="sort-posts">
-                <a className="sort-link" href="#">
-                    <hr className="sort-line" />
-                    Sort by:&nbsp;<span className="sort-selection">Top</span>
-                </a>
-
-                <div className="dropdown-menu">
-                    <a className="dropdown-item" href="#">Top</a>
-                    <a className="dropdown-item" href="#">Recent</a>
+                <div className="post-options">
+                    <button type="button" className="new-post-btn" onClick={handlePostSubmit}>
+                        <FaEdit />&nbsp;Post
+                    </button>
+                    <div className="media-buttons">
+                        <button type="button" className="share-media">
+                            <FaFileAlt />
+                        </button>
+                        <button type="button" className="share-media">
+                            <FaVideo />
+                        </button>
+                        <button type="button" className="share-media">
+                            <FaCamera />
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <br />
-
+            {/* Example Post Display */}
             <div className="show-post-container">
                 <div className="show-comment">
                     <b>Your connection&nbsp;</b>commented/reacted on this
                 </div>
                 <div className="show-account-name">
-                    <a href="#">
-                        <i className="fas fa-user-circle fa-4x" style={{ color: "rgba(0,0,0,.6)" }}></i>
-                        <div className="account-details">
-                            <b>User Name</b>&nbsp;
-                            <i className="fas fa-circle small-icon"></i>&nbsp;1st
-                            <br />
-                            <p>User bio</p>
-                            <p>
-                                8h <i className="fas fa-circle small-icon"></i> Edited/None{" "}
-                                <i className="fas fa-circle small-icon"></i> <i className="fas fa-globe"></i>
-                            </p>
-                        </div>
-                    </a>
+                    <FaUserCircle size="2rem" color="rgba(0, 0, 0, 0.6)" />
+                    <div className="account-details">
+                        <b>User Name</b>&nbsp;
+                        <span className="small-icon">●</span>&nbsp;1st
+                        <br />
+                        <p>User bio</p>
+                        <p>
+                            8h&nbsp;<span className="small-icon">●</span> Edited/None&nbsp;
+                            <span className="small-icon">●</span> <FaGlobe />
+                        </p>
+                    </div>
                 </div>
                 <div>
                     <p>Lorem Ipsum text describing the post goes here. This is where the content of the post is displayed.</p>
                 </div>
                 <div className="show-activity">
-                    <i className="fas fa-thumbs-up"></i>&nbsp;You and&nbsp;
-                    <a href="#"><b>n</b></a>&nbsp;others
+                    <FaThumbsUp />&nbsp;You and&nbsp;<a href="#"><b>n</b></a>&nbsp;others
                 </div>
             </div>
         </div>
