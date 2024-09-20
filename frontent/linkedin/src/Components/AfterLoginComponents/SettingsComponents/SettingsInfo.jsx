276c85280422f0953949947c96feeea70e0a7658
drammos <jrammos@outlook.com.gr>
2024-09-20 12:39:04 +0300

    Fixed the SettingsInfo Component

--- frontent/linkedin/src/Components/AfterLoginComponents/SettingsComponents/SettingsInfo.jsx
+++ frontent/linkedin/src/Components/AfterLoginComponents/SettingsComponents/SettingsInfo.jsx
@@ -1,37 +1,118 @@
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { GoXCircle, GoCheckCircle } from "react-icons/go";
+import { Eye, EyeOff } from 'lucide-react';
+import { useNavigate } from "react-router-dom";
+
+import { agents } from '../../../agents';
+import useService from "../../Services/useService";
+
 import "./SettingsInfo.css";
 
-
 const SettingsInfo = ({ userInfo, onSave }) => {
-  const [email, setEmail] = useState(userInfo.email);
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const [errorCode, setErrorCode] = useState(2);
 
+  const [email, setEmail] = useState('');
+  const [CurrentPassword, setCurrentPassword] = useState('');
+  const [NewPassword, setNewPassword] = useState('');
+
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  
+  
+  const inputFormData = new FormData();
+  const url = agents.localhost + agents.updateUser;
+  const navigate = useNavigate();
+
+  
+  
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password !== repeatPassword) {
-      alert('Οι κωδικοί δεν ταιριάζουν!');
+    setError('');
+
+    if(NewPassword.length===0 && (!email || email.length === 0 || email == userInfo.email)){
       return;
     }
-    onSave({ email, password });
+
+    if(CurrentPassword==null || CurrentPassword.length==0){
+      setError('Please complete the Current Password');
+      setMessage('Error');
+      setErrorCode(1);
+      return;
+    }
+
+    // Ελέγχουμε αν έχει γίνει κάποια αλλαγή
+    inputFormData.append('FirstName', userInfo.firstName);
+    inputFormData.append('LastName', userInfo.lastName);
+    inputFormData.append('PhoneNumber', userInfo.phoneNumber);
+    inputFormData.append('Email', email);
+    inputFormData.append('Username', userInfo.userName);
+    inputFormData.append('CurrentPassword', CurrentPassword);
+    inputFormData.append('PictureURL', userInfo.pictureURL);
+    inputFormData.append('CoverLetterURL', userInfo.coverLetterURL);
+    inputFormData.append('WebPage', userInfo.webPage);
+    if(NewPassword) inputFormData.append('NewPassword', NewPassword);
+
+    refetch();
   };
+
+  const { response, loading, refetch } = useService(
+    'Updating user...',
+    'PUT',
+    url,
+    inputFormData,
+    'multipart/form-data'
+  );
+
+  useEffect(() => {
+    if (userInfo?.email) {
+      setEmail(userInfo.email); // Set email when userInfo is available
+    }
+  }, [userInfo]);
+
+  useEffect(() => {
+    if (response) {
+        if (response.status === 200) {
+            userInfo.email = email;
+            setErrorCode(0);
+            setMessage('Account updated successfully!');
+            setTimeout(() => navigate(Routes.Home), 2000);
+        } else {
+            console.log(response.data.detail);
+            setError(response.data.detail);
+            setErrorCode(1);
+        }
+    }
+  }, [response, navigate, message]);
+
+  const togglePasswordVisibility = (field) => {
+    if (field === 'current') {
+      setShowCurrentPassword(!showCurrentPassword);
+    } else {
+      setShowNewPassword(!showNewPassword);
+    }
+  };
+
+
 
   return (
     <div className="settings-container">
-      <h2>Use Settings</h2>
+      <h2>User Settings</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Firstname</label>
-          <input type="text" value={userInfo.FirstName} disabled />
+          <input type="text" value={userInfo.firstName} disabled />
         </div>
         <div className="form-group">
           <label>Lastname</label>
-          <input type="text" value={userInfo.LastName} disabled />
+          <input type="text" value={userInfo.lastName} disabled />
         </div>
         <div className="form-group">
           <label>Username</label>
-          <input type="text" value={userInfo.username} disabled />
+          <input type="text" value={userInfo.userName} disabled />
         </div>
         <div className="form-group">
           <label>Email</label>
@@ -42,36 +123,49 @@ const SettingsInfo = ({ userInfo, onSave }) => {
           />
         </div>
         <div className="form-group">
-          <label>Τηλέφωνο</label>
+          <label>Phone Number</label>
           <input type="tel" value={userInfo.phoneNumber} disabled />
         </div>
         <div className="form-group">
-          <label>Ημερομηνία Γέννησης</label>
-          <input type="date" value={userInfo.birthDate} disabled />
+          <label>Birthday</label>
+          <input type="date" value={userInfo.birthday} disabled />
         </div>
         <div className="form-group">
-          <label>Νέος Κωδικός</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <label>Current Password</label>
+          <div className="password-input-wrapper">
+            <input
+              type={showCurrentPassword ? 'text' : 'password'}
+              value={CurrentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
+            <span 
+              className="password-toggle"
+              onClick={() => togglePasswordVisibility('current')}
+            >
+              {!showCurrentPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+            </span>
+          </div>
         </div>
         <div className="form-group">
-          <label>Επανάληψη Κωδικού</label>
-          <input
-            type="password"
-            value={repeatPassword}
-            onChange={(e) => setRepeatPassword(e.target.value)}
-          />
+          <label>New Password</label>
+          <div className="password-input-wrapper">
+            <input
+              type={showNewPassword ? 'text' : 'password'}
+              value={NewPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <span 
+              className="password-toggle"
+              onClick={() => togglePasswordVisibility('new')}
+            >
+              {!showNewPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+            </span>
+          </div>
         </div>
-        <div className="form-group">
-          <label>
-            <input type="checkbox" required />
-            Συμφωνώ με τους όρους και τις προϋποθέσεις
-          </label>
+        <div className={errorCode === 1 ? 'error-message' : (errorCode === 0 ? 'success-message' : '')}>
+          {errorCode === 1 ? <><GoXCircle /> {error}</> : (errorCode === 0 ? <><GoCheckCircle /> {message}</> : '')}
         </div>
-        <button type="submit" className="save-button">Αποθήκευση Αλλαγών</button>
+        <button type="submit" className="save-button">Save</button>
       </form>
     </div>
   );
