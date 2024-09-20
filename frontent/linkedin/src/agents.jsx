1749019edacb8c4f6c221fbe0734ee5ea18f49e5
tminaidis9 <theomin2001@gmail.com>
2024-09-21 02:18:18 +0300

    create some new api connections

--- frontent/linkedin/src/agents.jsx
+++ frontent/linkedin/src/agents.jsx
@@ -8,9 +8,63 @@ export const agents = {
     getListFromInComingRequests: 'User/GetListFromInComingRequests',
     deleteUser: 'User/DeleteUser',
     getAllUsers: 'User/GetAllUsers',
+    getRoleForUser: 'User/GetRoleForUser ',
+    isConnectedUsers: 'User/IsConnectedUsers ',
+    isPendingRequest: 'User/IsPendingRequest ',
+    isInComingRequest: 'User/IsInComingRequest ',
+    requestToConnection: 'User/RequestToConnection ',
+    acceptRequest: 'User/AcceptRequest ',
+    deleteRequest: 'User/DeleteRequest',
+    getListFromConnections: 'User/GetListFromConnections ',
+    getListFromPendingRequests: 'User/GetListFromPendingRequests ',
+
     // Post
     createPost: 'Post/CreatePost',
     getUserPosts: 'Post/GetUserPosts',
-    getPostsFromConnectedUsers: 'Post/GetPostsFromConnectedUsers'
-    
+    editPost: 'Post/EditPost',
+    deletePost: 'Post/DeletePost',
+    createComment: 'Post/CreateComment',
+    getPostComments: 'Post/GetPostComments',
+    getUserComments: 'Post/GetUserComments',
+    deleteAllCommentsFromPost: 'Post/DeleteAllCommentsFromPost',
+    deleteComment: 'Post/DeleteComment',
+    createReaction: 'Post/CreateReaction',
+    getPostReactions: 'Post/GetPostReactions',
+    getUserReactions: 'Post/GetUserReactions',
+    deleteAllReactionsFromPost: 'Post/DeleteAllReactionsFromPost',
+    deleteReaction: 'Post/DeleteReaction',
+    getPostsFromConnectedUsers: 'Post/GetPostsFromConnectedUsers',
+
+
+    // Job
+    createJob: 'Job/CreateJob',
+    editJob: 'Job/EditJob',
+    deleteJob: 'Job/DeleteJob',
+    getJob: 'Job/GetJob',
+    getAllJobs: 'Job/GetAllJobs',
+    getAllOpenJobs: 'Job/GetAllOpenJobs',
+    getAllCloseJobs: 'Job/GetAllCloseJobs',
+    applyForJob: 'Job/ApplyForJob',
+    getUserPostedJobs: 'Job/GetUserPostedJobs',
+    getAppliedUserJobs: 'Job/GetAppliedUserJobs',
+    getUserStatusJobs: 'Job/GetUserStatusJobs',
+    acceptedJobApplication: 'Job/AcceptedJobApplication',
+    rejectJobApplication: 'Job/RejectJobApplication',
+    withdrawnJobApplication: 'Job/WithdrawnJobApplication',
+    getFilteredJobs: 'Job/GetFilteredJobs',
+    closeJob: 'Job/CloseJob',
+
+    // Experience
+    addExperience: 'Experience/AddExperience',
+    getExperiences: 'Experience/GetExperiences',
+    getExperience: 'Experience/GetExperience',
+    editExperience: 'Experience/EditExperience',
+    deleteExperience: 'Experience/DeleteExperience',
+
+    // Education
+    addEducation: 'Education/AddEducation',
+    getEducations: 'Education/GetEducations',
+    getEducation: 'Education/GetEducation',
+    editEducation: 'Education/EditEducation',
+    deleteEducation: 'Education/DeleteEducation'
 };
