1c50f1d334c1d601ae224d13b693e84568d9e9c8
tminaidis9 <theomin2001@gmail.com>
2024-09-23 01:04:00 +0300

    added age on MainPageLeft

--- frontent/linkedin/src/Components/AfterLoginComponents/MainPageElements/MainPageLeft.jsx
+++ frontent/linkedin/src/Components/AfterLoginComponents/MainPageElements/MainPageLeft.jsx
@@ -3,6 +3,20 @@ import PropTypes from 'prop-types';
 import './styles/MainPageLeft.css';
 
 const MainPageLeft = ({ userInfo }) => {
+
+    const calculateAge = (birthday) => {
+        const birthDate = new Date(birthday);
+        const today = new Date();
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate()))
+            age--;
+
+        return age;
+    };
+
     // if name and surname doesn't start with capitals, fix it
     const lastname = userInfo.lastName  ? userInfo.lastName.charAt(0).toUpperCase() + userInfo.lastName.slice(1).toLowerCase() : '';
     const firstname = userInfo.firstName ? userInfo.firstName.charAt(0).toUpperCase() + userInfo.firstName.slice(1).toLowerCase() : '';
@@ -10,12 +24,16 @@ const MainPageLeft = ({ userInfo }) => {
     const initials = firstname.charAt(0) + lastname.charAt(0);
     const [education, setEducation] = useState('');
 
-    useEffect(() => {
-        if (userInfo.educations !== [])
-            setEducation(userInfo.educations);
-        else
-            setEducation(null);
-    }, [userInfo.educations]);
+    const bday = userInfo.birthday;
+    const age = calculateAge(bday);
+
+
+    //useEffect(() => {
+    //    if (userInfo.educations !== [])
+    //        setEducation(userInfo.educations);
+    //    else
+    //        setEducation(null);
+    //}, [userInfo.educations]);
 
     return (
         <div className="main-page-left">
@@ -29,7 +47,8 @@ const MainPageLeft = ({ userInfo }) => {
                 </div>
                 <div className="profile-info">
                     <b>{firstname} {lastname}</b>
-                    <p>{education} Physiotherapist</p>
+                    <>Age: {age}</>
+                    {/*<p>{education} Physiotherapist</p>*/}
                 </div>
             </div>
             <div className="quick-links-section">
@@ -46,7 +65,8 @@ MainPageLeft.propTypes = {
         firstName: PropTypes.string,
         lastName: PropTypes.string,
         pictureURL: PropTypes.string,
-        educations: PropTypes.string
+        educations: PropTypes.string,
+        birthday: PropTypes.string
     }).isRequired,
 };
 
