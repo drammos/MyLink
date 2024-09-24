598b05cb5a7ed41e9c5cfae2c4115cdf04e3d795
drammos <jrammos@outlook.com.gr>
2024-09-24 03:15:05 +0300

    Update LoggedinNavBar jsx and css for searchbar and dropdown

--- frontent/linkedin/src/Components/AfterLoginComponents/LoggedInNavBar/LoggedInNavBar.jsx
+++ frontent/linkedin/src/Components/AfterLoginComponents/LoggedInNavBar/LoggedInNavBar.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoggedInNavBar.css';
 import logo from '../../../assets/Logo.png';
 import ProfilePopup from './ProfilePopup/ProfilePopup';
@@ -6,11 +7,28 @@ import NotificationPopup from './NotificationPopup/NotificationPopup';
 import { useNavigationHelpers } from '../Helpers/useNavigationHelpers';
 import useGetListFromIncomingRequests from '../../Services/useGetListFromIncomingRequests';
 import PropTypes from 'prop-types';
+import useService from '../../Services/useService';
+import {agents} from '../../../agents'; 
 
 const LoggedInNavBar = ({ userInfo }) => {
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-    const [isNotificationOpen, setIsNotificationOpen] = useState(false); // State for notification popup
+    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
     const [userPhotoUrl, setuserPhotoUrl] = useState('');
+    const [users, setUsers] = useState([]);
+    const [count, setCount] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(6);
+    const navigate = useNavigate();
+    //Metadata
+    const [metadata, setMetadata] = useState({
+        currentPage: 1,
+        totalPages: 1,
+        pageSize: 10,
+        totalCount: 10
+      });  
 
     const {
         handleHomeButton,
@@ -31,9 +49,27 @@ const LoggedInNavBar = ({ userInfo }) => {
         if (isProfileMenuOpen)
             setIsProfileMenuOpen(false);
     };
-
+    
     const defaultPhotoURL = 'https://res.cloudinary.com/dvhi4yyrm/image/upload/v1725693786/bui1pzeaj5msljlp1qvi.png';
     const { listLength, notificationList, refetch } = useGetListFromIncomingRequests();
+
+    const buildUrl = useCallback(() => {
+        const params = new URLSearchParams();
+        const url2 = agents.localhost + agents.searchUsers;
+        params.append('SearchName', searchTerm.toString());
+        params.append('PageNumber', pageNumber.toString());
+        params.append('PageSize', pageSize.toString());
+        return `${url2}?${params}`;
+      }, [searchTerm, pageNumber, pageSize]);
+    
+    const {response, loading, refetch: refetchService} = useService(
+        'Loading users..',
+        'GET',
+        buildUrl(),
+        null,  
+        undefined,
+        true
+    );
 
     useEffect(() => {
         if (userInfo.pictureURL && userInfo.pictureURL !== "null" && userInfo.pictureURL !== '') {
@@ -55,9 +91,53 @@ const LoggedInNavBar = ({ userInfo }) => {
 
     }, [userInfo, refetch]);
 
+    useEffect(() => {
+        if (searchTerm.length > 0) {
+            refetchService();
+        } else {
+            setShowDropdown(false);
+        }
+    }, [refetchService, pageNumber, pageSize]); 
+
+    const handleSearchChange = async (event) => {
+        const term = event.target.value;       
+        setSearchTerm(term);
+        refetchService(); 
+        setShowDropdown(term.length > 0); 
+    };
+    
+
+    useEffect(() => {
+        if (response) {
+            if (response.status === 200) {
+                console.log("rewsp", response.data);
+                setUsers(response.data); 
+                setSearchResults(response.data); 
+                
+                const paginationJson = JSON.parse(response.headersDict['pagination']);
+                setMetadata(paginationJson);
+                setCount(response.data.length);
+    
+                setShowDropdown(true);
+            }
+        } else if (!loading) {
+            setShowDropdown(false); 
+        }
+    }, [response, loading, pageNumber, pageSize]);
+    
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        navigate(`/search?term=${searchTerm}`);
+    };
+
+    const handleSeeAllResults = () => {
+        navigate(`/search?term=${searchTerm}`);
+    };
+
     return (
         <div className="loggedInNavbarContainer">
-            <div className="navbar-left">
+             <div className="navbar-left">
                 <div className="logo">
                     <img src={logo} alt="Logo" />
                 </div>
@@ -67,8 +147,30 @@ const LoggedInNavBar = ({ userInfo }) => {
                 <div className="nav-item" onClick={handleInterestsButton}>Messages</div>
             </div>
             <div className="navbar-center">
-                <input type="text" placeholder="Search" />
-                <button type="submit">Search</button>
+                <form onSubmit={handleSearchSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="Search" 
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                </form>
+                {showDropdown && (
+                    <div className="search-dropdown">
+                        {searchResults.map((result, index) => (
+                            <div key={index} className="search-result-item">
+                            <img src={result.pictureURL || defaultPhotoURL} alt={result.name} />
+                            <div className="result-info"> {}
+                                <span className="fullname">{result.firstName + " " + result.lastName}</span>
+                                <span>{result.userName}</span>
+                            </div>
+                        </div>
+                        ))}
+                        <div className="see-all-results" onClick={handleSeeAllResults}>
+                            See all results
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="navbar-right">
                 <button className="notification-button" onClick={toggleNotificationPopup}>
