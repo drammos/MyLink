38d36e3fae178b6d9b7aec50f012527e745838eb
drammos <jrammos@outlook.com.gr>
2024-09-24 10:58:03 +0300

    Add searchUsers.jsx

--- frontent/linkedin/src/Pages/SearchUsers/SearchUsers.jsx
+++ frontent/linkedin/src/Pages/SearchUsers/SearchUsers.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import LoggedInNavBar from "../../Components/AfterLoginComponents/LoggedInNavBar/LoggedInNavBar";
 import Footer from "../../Components/Footer/Footer";
-import SearchResults from "../../Components/AfterLoginComponents/SearchResultsComponent/SearchResult";
-import PageNotFound from "../PageNotFound/PageNotFound";
+import SearchResults from "../../Components/AfterLoginComponents/SearchResultsComponent/SearchResults.jsx";
+import useGetUser from '../../Components/Services/useGetUser.jsx';
 import './SearchUsers.css';
 
 const useDocumentTitle = (title) => {
@@ -11,10 +13,18 @@ const useDocumentTitle = (title) => {
 };
 
 const SearchUsers = () => {
+    const { userInfo } = useGetUser();
+    
+    // Use useSearchParams to retrieve the term from the URL
+    const [searchParams] = useSearchParams();
+    const term = searchParams.get('term');  // This will retrieve the ?term= from the URL
+
+    useDocumentTitle(`Search Results for: ${term}`);
 
     return (
         <div>
-            <SearchResults items={items} />
+            <LoggedInNavBar userInfo={userInfo} />
+            <SearchResults searchTerm={term} />
             <Footer />
         </div>
     );
