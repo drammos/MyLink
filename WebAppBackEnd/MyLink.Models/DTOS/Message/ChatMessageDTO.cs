5340c21bc01c12da4c41d0686fca2fe3d3be8d2a
drammos <jrammos@outlook.com.gr>
2024-09-25 03:00:13 +0300

    Add new DTOs for the Messages and discussions

---
+++ WebAppBackEnd/MyLink.Models/DTOS/Message/ChatMessageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLink.Models.DTOS
+{
+    public class ChatMessageDTO
+    {
+        public string OwnerUsername { get; set; }
+        public string OwnerId { get; set; }
+        public string MessageBody { get; set; }
+    }    
+}
---
+++ WebAppBackEnd/MyLink.Models/DTOS/Message/MessageDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLink.Models.DTOS
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+        public string? MessageBody { get; set; }
+        
+        //Recipient User
+        public string? ReceiptUserId { get; set; }
+        public string? ReceiptUsername { get; set; }
+        public string? ReceiptPictureURL {get; set;}
+        
+        //Sender User
+        public string? SenderUserId { get; set; }
+        public string? SenderUsername{ get; set; }
+        public string? SenderPictureURL { get; set; }
+    }    
+}
