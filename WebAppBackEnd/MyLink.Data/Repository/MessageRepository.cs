a6290c70e04e48d999dd688cd6a991a24f38c400
drammos <jrammos@outlook.com.gr>
2024-09-25 03:00:38 +0300

    Create Message Repository with 2 functions

---
+++ WebAppBackEnd/MyLink.Data/Repository/MessageRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLink.Data.Repository.IRepository;
+using MyLink.Models;
+using MyLink.Data.Access;
+
+namespace MyLink.Data.Repository
+{
+    public class MessageRepository: RepositoryBase<Message>, IMessageRepository
+    {
+        private ApplicationDbContext _context;
+
+        public MessageRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Message GetMessageById(int id)
+        {
+            return _context.Messages
+                    .Include(u => u.User)
+                    .FirstOrDefault(m => m.Id == id);
+        }
+
+        public IQueryable<Message> GetDiscussion(User myUser, User interactedUser)
+        {
+            return _context.Messages
+                .Where(
+                    message => 
+                        (message.SenderUsername == myUser.UserName && message.UserId == interactedUser.Id) ||
+                        (message.SenderUsername == interactedUser.UserName && message.UserId == myUser.Id)
+                    )
+                .OrderByDescending(message => message.DateCreated)
+                .Include(message => message.User);
+            
+        }
+    }    
+}
