79b0dc07f995426a356c77c52214f668333b93c3
drammos <jrammos@outlook.com.gr>
2024-09-25 03:00:13 +0300

    Add new DTOs for the Messages and discussions

---
+++ WebAppBackEnd/MyLink.Models/DTOS/Message/CreateMessageDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLink.Models.DTOS
+{
+    public class CreateMessageDTO
+    {
+        [Required]
+        public string? MessageBody { get; set; }
+        [Required]
+        public string? SenderUsername { get; set; }
+        [Required]
+        public string? RecipientUsername { get; set; }
+    }
+}
