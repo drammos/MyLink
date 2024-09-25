4f5262123158188b5af69231ad660afc346f8e47
drammos <jrammos@outlook.com.gr>
2024-09-26 01:08:56 +0300

    Add new DTO for chats

---
+++ WebAppBackEnd/MyLink.Models/DTOS/Message/ChatOutDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLink.Models.DTOS
+{
+    public class ChatOutDTO
+    {
+        public string InterlocutorUsename{ get; set; }
+        public string InterlocutorPictureURL { get; set; }
+        public string InterlocutorFirstname { get; set; }
+        public string InterlocutorLastname { get; set; }
+        public string LastMessage { get; set; }
+    }    
+}
