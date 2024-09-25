02b746beae121d43a5eb391b730b5435faf49357
drammos <jrammos@outlook.com.gr>
2024-09-25 03:00:38 +0300

    Create Message Repository with 2 functions

---
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyLink.Models;
+
+namespace MyLink.Data.Repository.IRepository
+{
+    public interface IMessageRepository: IRepositoryBase<Message>
+    {
+        public Message GetMessageById(int id);
+        public IQueryable<Message> GetDiscussion(User myUser,User interactedUser);
+    }
+}
