94cb04fd361fbefdca9ad4af257f49e3e480c350
drammos <jrammos@outlook.com.gr>
2024-09-26 01:09:32 +0300

    Update messages pages

--- MyLinkFrontEnd/src/Pages/Messages/Messages.jsx
+++ MyLinkFrontEnd/src/Pages/Messages/Messages.jsx
@@ -1,31 +1,19 @@
-import React, { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import React from "react";
 import LoggedInNavBar from "../../Components/AfterLoginComponents/LoggedInNavBar/LoggedInNavBar.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
 import useGetUser from '../../Components/Services/useGetUser.jsx';
-import UserMessages from "../../Components/AfterLoginComponents/MessagesComponents/UserMessages.jsx"
+import MessageInterface from "../../Components/AfterLoginComponents/MessagesComponents/MessageInterface.jsx";
 import './Messages.css';
-
-const useDocumentTitle = (title) => {
-    useEffect(() => {
-        document.title = title;
-    }, [title]);
-};
 
 const Messages = () => {
     const { userInfo } = useGetUser();
     
-    // Use useSearchParams to retrieve the term from the URL
-    const [searchParams] = useSearchParams();
-    const term = searchParams.get('term');  // This will retrieve the ?term= from the URL
-
-    useDocumentTitle(`Search Results for: ${term}`);
-
     return (
-        <div>
+        <div className="messages-page">
             <LoggedInNavBar userInfo={userInfo} />
-            <UserMessages userInfo={userInfo} />
-            <Footer />
+            <div className="messages-content">
+                <MessageInterface userInfo={userInfo} />
+            </div>
         </div>
     );
 };
