cdda68ddf1ad6654430183f040bae046c293b92b
drammos <jrammos@outlook.com.gr>
2024-09-27 01:15:21 +0300

    Create MatrixFactorizationBackEndService

---
+++ WebAppBackEnd/MyLink.Services/MatrixFactorization/MatrixBackgroundService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MyLink.Services.MatrixFactorization
+{
+
+    public class MatrixBackgroundService: BackgroundService
+    {
+
+    }
+}
