146c90baef48b2b6368ea2ac8aa5110dd887b8f2
drammos <jrammos@outlook.com.gr>
2024-09-27 23:44:45 +0300

    Corrections

--- MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageEducationInfo.jsx
+++ MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageEducationInfo.jsx
@@ -1,24 +1,65 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { agents } from '../../../agents';
+import './styles/UsersPageExperienceInfo.css';
+import useService from '../../Services/useService';
 
 const UsersPageExperienceInfo = ({ userInfo }) => {
-    const educations = userInfo.educations || [];
+    const [educations, setEducations] = useState([]);
+
+    const [error, setError] = useState('');
+  
+    console.log("id:",userInfo);
+    const url = agents.localhost + agents.getEducations + '?UserId=' + userInfo.id;
+    const { response, loading, refetch } = useService(
+      'Getting all educations ...',
+      'GET',
+      url,
+      null,
+      undefined,
+      true
+    );
+  
+    useEffect(() => {
+      refetch();
+    }, [userInfo]);
+  
+  
+    useEffect(() => {
+      if (response) {
+        if (response.status === 200) {
+            setEducations(response.data);
+        } else {
+            console.log(response.data.detail);
+            setError(response.data.detail);
+        }
+      }
+    }, [response]);
+  
+  
+
+  
     return (
-        <div className="education-info-container">
-            <h3>Education</h3>
-            {educations.length > 0 ? (
-                <ul>
-                    {educations.map((education, index) => (
-                        <li key={index}>
-                            <p>{education.degree} - {education.institution}</p>
-                            <p>{education.startDate} to {education.endDate}</p>
-                        </li>
-                    ))}
+        <div className="education-component">
+            <h2>Education</h2>
+            {error && <p className="error">{error}</p>}
+            <div className="education-list-container">
+                <ul className="education-list">
+                {educations.map((edu, index) => (
+                    <li key={index} className="education-item">
+                    <h3>School: {edu.school}</h3>
+                    <p>Degree: {edu.degree} </p>
+                    <p>Field of Study: {edu.fieldOfStudy}</p>
+                    <p>{new Date(edu.startDate).toLocaleDateString()} - {edu.endDate ? new Date(edu.endDate).toLocaleDateString() : 'Present'}</p>
+                    {edu.grade && <p>Grade: {edu.grade}</p>}
+                    {edu.description && <p>{edu.description}</p>}
+                    </li>
+                ))}
                 </ul>
-            ) : (
-                <p>No education information available</p>
-            )}
+            </div>
         </div>
+
+      
     );
 };
 
--- MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageExperienceInfo.jsx
+++ MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageExperienceInfo.jsx
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { agents } from '../../../agents';
+import './styles/ExperienceInfo.css';
+import useService from '../../Services/useService';
 
 const ExperienceInfo = ({ userInfo }) => {
-    const experiences = userInfo.experiences || []; // Extract experiences from userInfo
-
-    return (
-        <div className="experience-info-container">
-            <h3>Experience</h3>
-            {experiences.length > 0 ? (
-                <ul>
-                    {experiences.map((experience, index) => (
-                        <li key={index}>
-                            <p>{experience.jobTitle} - {experience.company}</p>
-                            <p>{experience.startDate} to {experience.endDate}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No experience information available</p>
-            )}
-        </div>
+    const [experiences, setExperiences] = useState([]);
+    const [error, setError] = useState('');
+  
+    const url = agents.localhost + agents.getExperiences + '?UserId=' + userInfo.id;
+    const { response, loading, refetch } = useService(
+      'Getting all Experiences ...',
+      'GET',
+      url,
+      null,
+      undefined,
+      true
     );
-};
-
+  
+    useEffect(() => {
+      refetch();
+    }, [userInfo]);
+  
+    useEffect(() => {
+      if (response) {
+        if (response.status === 200) {
+          setExperiences(response.data);
+        } else {
+          console.log(response.data.detail);
+          setError(response.data.detail);
+        }
+      }
+    }, [response]);
+  
+    return (
+      <div className="experience-component">
+        <h2>Experience</h2>
+        {error && <p className="error">{error}</p>}
+        <div className="experience-content">
+          <div className="experience-list-container">
+            <ul className="experience-list">
+              {experiences.map((exp, index) => (
+                <li key={index} className="experience-item">
+                  <h3>Title: {exp.title}</h3>
+                  <p>Company Name: {exp.companyName}</p>
+                  <p>Employment Type: {exp.employmentType}</p>
+                  <p>{new Date(exp.startDate).toLocaleDateString()} - {exp.currentJob ? 'Present' : new Date(exp.endDate).toLocaleDateString()}</p>
+                  <p>Location Type: {exp.locationType}</p>
+                  <p>Location: {exp.location}</p>
+                  {exp.description && <p>{exp.description}</p>}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
 ExperienceInfo.propTypes = {
     userInfo: PropTypes.shape({
-        experiences: PropTypes.arrayOf(PropTypes.shape({
-            jobTitle: PropTypes.string,
-            company: PropTypes.string,
-            startDate: PropTypes.string,
-            endDate: PropTypes.string,
-        }))
+      id: PropTypes.string.isRequired,
     }).isRequired,
 };
 
