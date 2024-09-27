e196532eeb9be7844b1b44ed45970f25a9972979
tminaidis9 <theomin2001@gmail.com>
2024-09-27 20:56:44 +0300

    fix notifications,css

--- MyLinkFrontEnd/src/Components/AfterLoginComponents/LoggedInNavBar/NotificationPopup/NotificationPopup.jsx
+++ MyLinkFrontEnd/src/Components/AfterLoginComponents/LoggedInNavBar/NotificationPopup/NotificationPopup.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './NotificationPopup.css';
 
 const NotificationPopup = ({ notifications }) => {
+    useEffect(() => {
+        console.log("Notifications are: ", notifications);
+    }, [notifications]);
+
     return (
         <div className="notification-popup">
             <h3>Notifications</h3>
             <ul>
                 {notifications.length > 0 ? (
                     notifications.map((notification, index) => (
-                        <li key={index}>{notification.message}</li>
+                        <li key={index} className="notification-item">
+                            <img
+                                src={notification.pictureURL}
+                                alt={`${notification.firstName} ${notification.lastName}`}
+                                className="notification-image"
+                            />
+                            <span>{notification.firstName} {notification.lastName} sends you a friend request</span>
+                        </li>
                     ))
                 ) : (
-                    <li>No new notifications</li>
+                    <li>No new notifications</li> 
                 )}
             </ul>
         </div>
@@ -22,7 +33,9 @@ const NotificationPopup = ({ notifications }) => {
 NotificationPopup.propTypes = {
     notifications: PropTypes.arrayOf(
         PropTypes.shape({
-            message: PropTypes.string.isRequired,
+            firstName: PropTypes.string.isRequired,
+            lastName: PropTypes.string.isRequired,
+            pictureURL: PropTypes.string.isRequired,
         })
     ).isRequired,
 };
