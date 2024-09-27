801651e980a9541689eb3a44b024a2d797b1005f
drammos <jrammos@outlook.com.gr>
2024-09-28 02:45:07 +0300

    Add new models

---
+++ WebAppBackEnd/MyLink.Models/ViewedPosts.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLink.Models{
+        public class ViewedPosts
+        {
+                [Required]
+                public int Id { get; set; }
+
+                [Required]
+                public string? Username { get; set; }
+
+                [Required]
+                public int PostId { get; set; }
+                [ForeignKey("PostId")]
+                public Post Post { get; set; } = null!;
+        }
+}
