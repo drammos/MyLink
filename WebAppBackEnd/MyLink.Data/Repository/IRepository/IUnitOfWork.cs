3ef6088d96db1e96be9dad2614a5dee79c4edfef
drammos <jrammos@outlook.com.gr>
2024-09-28 02:45:52 +0300

    Add new repository

--- WebAppBackEnd/MyLink.Data/Repository/IRepository/IUnitOfWork.cs
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace MyLink.Data.Repository.IRepository
         public IPostRepository Post { get; set; }
         public IJobRepository Job { get; set; }
         public IMessageRepository Message { get; set; }
+        public IViewedPostsRepository ViewedPosts { get; }
         void Save();
     }
 }
