ad25aa857fc82f7c09f49d568b64ae7b1f5ebbfb
drammos <jrammos@outlook.com.gr>
2024-09-28 02:44:46 +0300

    Add new controller for ViewedPosts

---
+++ WebAppBackEnd/WebApplicationMyLink/Controllers/ViewedPostsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLink.Data.Repository.IRepository;
+using MyLink.Models;
+using Microsoft.AspNetCore.Identity;
+using MyLink.Models;
+using MyLink.Models.DTOS;
+
+namespace WebAppMyLink.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ViewedPostsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+
+        public ViewedPostsController(IUnitOfWork unitOfWork, UserManager<User> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        [HttpPost("PostViewedPosts")]
+        public async Task<IActionResult> PostViewedPosts([FromForm] ViewedPostDTO viewedPostDTO)
+        {
+            User user = await _userManager.FindByIdAsync(viewedPostDTO.UserId);
+            if(user==null) return NotFound();
+            ViewedPosts viewedPost = new ViewedPosts
+            {
+                Username = user.UserName, 
+                PostId = viewedPostDTO.PostId,   
+            };
+            _unitOfWork.ViewedPosts.Add(viewedPost);
+            _unitOfWork.Save();
+            return Ok();
+        }
+
+       
+    }
+}
