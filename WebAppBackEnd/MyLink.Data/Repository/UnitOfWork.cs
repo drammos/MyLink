0668c541ae22c05880d50ac86a05875cf807e3ed
drammos <jrammos@outlook.com.gr>
2024-09-28 02:45:52 +0300

    Add new repository

--- WebAppBackEnd/MyLink.Data/Repository/UnitOfWork.cs
+++ WebAppBackEnd/MyLink.Data/Repository/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using MyLink.Data.Repository.IRepository;
 using MyLink.Data.Access;
+using MyLink.Models;
 
 namespace MyLink.Data.Repository
 {
@@ -12,6 +13,7 @@ namespace MyLink.Data.Repository
         public IPostRepository Post { get; set; }
         public IJobRepository Job { get; set; }
         public IMessageRepository Message { get; set; }
+        public IViewedPostsRepository ViewedPosts { get; set; }
 
         public UnitOfWork(ApplicationDbContext context)
         {
@@ -22,6 +24,7 @@ namespace MyLink.Data.Repository
             Post = new PostRepository(context);
             Job = new JobRepository(context);
             Message = new MessageRepository(context);
+            ViewedPosts = new ViewedPostsRepository(context);
         }
 
         public void Save()
---
+++ WebAppBackEnd/MyLink.Data/Repository/ViewedPostsRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyLink.Data.Repository.IRepository;
+using MyLink.Models;
+using MyLink.Data.Access;
+
+namespace MyLink.Data.Repository;
+
+public class ViewedPostsRepository : RepositoryBase<ViewedPosts>, IViewedPostsRepository
+{
+    private ApplicationDbContext _context;
+    public ViewedPostsRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
