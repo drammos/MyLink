bd83cfc54a4fbea0ce02acaf9eedc3d5f5bcbd33
tminaidis9 <theomin2001@gmail.com>
2024-09-28 02:37:08 +0300

    add button for connected on usersPage, fix css

---
+++ MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/ConnectButton.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from '../../../routes';
+
+const ConnectButton = ({ status, userId, handleConnectClick, requestToConnectLoading }) => {
+
+    const typeDictionary = {
+        Connected: 'Send Message',
+        NotConnected: 'Connect',
+        Pending: 'Pending',
+        InComing: 'Incoming'
+    };
+
+    const isDisabled = typeDictionary[status] === 'Incoming' || requestToConnectLoading;
+    const type = typeDictionary[status];
+    const navigate = useNavigate();
+
+
+    const handleClick = () => {
+        if (typeDictionary[status] === 'Connect')
+            handleConnectClick(userId);
+        else if (typeDictionary[status] === 'Send Message')
+            navigate(Routes.Messages);
+    };
+
+    useEffect(() => {
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",status);
+    }, []);
+
+    return (
+        <>
+            {status !== null && (
+                <button
+                    className="connect-button"
+                    onClick={handleClick}
+                    disabled={isDisabled}
+                >
+                    {type}
+                </button>
+            )}
+        </>
+    );
+}
+
+// PropTypes validation
+ConnectButton.propTypes = {
+    status: PropTypes.object.isRequired,
+    userId: PropTypes.string.isRequired,
+    handleConnectClick: PropTypes.func.isRequired,
+    requestToConnectLoading: PropTypes.bool.isRequired,
+    typeDictionary: PropTypes.object.isRequired,
+};
+
+export default ConnectButton;
--- MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageMainInfo.jsx
+++ MyLinkFrontEnd/src/Components/AfterLoginComponents/UsersPage/UsersPageMainInfo.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import useGetCommunicationType from '../../Services/User/useGetCommunicationType';
+import useRequestToConnections from '../../Services/User/useRequestToConnections';
+import ConnectButton from './ConnectButton';
+import './styles/UsersPageMainInfo.css'; 
 
 const UsersPageMainInfo = ({ userInfo }) => {
+    const [status, setStatus] = useState('NotConnected');
+    const { requestToConnectMessage, errorCode: requestToConnectErrorCode,
+        loading: requestToConnectLoading, requestToConnectRefetch } = useRequestToConnections();
+
+    const loggedInUserId = localStorage.getItem('id');
+    const loggedInUserRole = localStorage.getItem('role');
+
+    const { CommunicationTypeResponse, CommunicationTypeRefetch } = useGetCommunicationType();
 
     const lastname = userInfo.lastName ? userInfo.lastName.charAt(0).toUpperCase() + userInfo.lastName.slice(1).toLowerCase() : '';
     const firstname = userInfo.firstName ? userInfo.firstName.charAt(0).toUpperCase() + userInfo.firstName.slice(1).toLowerCase() : '';
+
+    useEffect(() => {
+        const fetchStatuses = async () => {
+            console.log("user.id = ", userInfo.id);
+            if (userInfo.id && loggedInUserId !== userInfo.id && loggedInUserRole !== 'Admin')
+                await CommunicationTypeRefetch(loggedInUserId, userInfo.id);
+            else
+                setStatus(null);
+        };
+        fetchStatuses();
+    }, [userInfo, userInfo.id]);
+
+    useEffect(() => {
+        if (CommunicationTypeResponse) {
+            setStatus(CommunicationTypeResponse.data.result);
+        }
+    }, [CommunicationTypeResponse]);
+
+    const handleConnectClick = async (userId) => {
+        await requestToConnectRefetch(userInfo.id, userId);
+        setStatus('Pending');
+    };
 
     return (
         <div className="main-info-container">
@@ -23,6 +57,8 @@ const UsersPageMainInfo = ({ userInfo }) => {
                 <p>Phone: {userInfo.phoneNumber}</p>
                 <p>Birthday: {userInfo.birthday}</p>
             </div>
+            <ConnectButton status={status} userId={userInfo.id}
+                handleConnectClick={handleConnectClick} requestToConnectLoading={requestToConnectLoading} /> 
         </div>
     );
 };
@@ -35,6 +71,7 @@ UsersPageMainInfo.propTypes = {
         email: PropTypes.string.isRequired,
         phoneNumber: PropTypes.string.isRequired,
         birthday: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
     }).isRequired,
 };
 
