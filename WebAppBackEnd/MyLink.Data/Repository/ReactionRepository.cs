6bcfebadd50d67990ed34940f6ab66f11dd1b836
drammos <jrammos@outlook.com.gr>
2024-09-28 12:24:04 +0300

    Create the Reaction Repository

---
+++ WebAppBackEnd/MyLink.Data/Repository/ReactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyLink.Data.Access;
+using MyLink.Models;
+using MyLink.Data.Repository.IRepository;
+namespace MyLink.Data.Repository;
+
+
+public class ReactionRepository: RepositoryBase<Reaction>, IReactionRepository
+{
+    private ApplicationDbContext _context;
+    
+    public ReactionRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
