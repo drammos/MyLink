8f38db21d0cd5fa76395c839ec0bc31fe05a62f3
drammos <jrammos@outlook.com.gr>
2024-09-28 12:24:19 +0300

    Create Comment Repository

---
+++ WebAppBackEnd/MyLink.Data/Repository/CommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyLink.Data.Access;
+using MyLink.Data.Repository.IRepository;
+using MyLink.Models;
+
+namespace MyLink.Data.Repository;
+
+public class CommentRepository: RepositoryBase<Comment>, ICommentRepository
+{
+    private ApplicationDbContext _context;
+    public CommentRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
