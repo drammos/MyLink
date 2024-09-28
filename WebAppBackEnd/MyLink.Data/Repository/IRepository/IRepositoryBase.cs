80c880add0c0b53db6a8c4652cb85e7193ad4d0f
drammos <jrammos@outlook.com.gr>
2024-09-28 12:23:48 +0300

    Add parameters in GetAll for filters

--- WebAppBackEnd/MyLink.Data/Repository/IRepository/IRepositoryBase.cs
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IRepositoryBase.cs
@@ -6,7 +6,7 @@ namespace MyLink.Data.Repository.IRepository
 {
     public interface IRepositoryBase<T> where T : class
     {
-        public IEnumerable<T> GetAll();
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null);
         public IQueryable<T> GetAllIQueryable();
         public T FirstOrDefault(Expression<Func<T, bool>> filter);
         public void Add(T entity);
