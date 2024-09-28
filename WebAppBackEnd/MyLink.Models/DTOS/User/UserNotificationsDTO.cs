c2f8154c9b9c27e32879ef71a5a058b4363092b8
drammos <jrammos@outlook.com.gr>
2024-09-29 02:12:53 +0300

    Create new DTO for notifications

---
+++ WebAppBackEnd/MyLink.Models/DTOS/User/UserNotificationsDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLink.Models.DTOS
+{
+    public class UserNotificationsDTO
+    {
+        public string? UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? PictureURL { get; set; }
+        public bool IsComment { get; set; }
+        public int PostId { get; set; }
+        public string? Body { get; set; }
+    }
+}
