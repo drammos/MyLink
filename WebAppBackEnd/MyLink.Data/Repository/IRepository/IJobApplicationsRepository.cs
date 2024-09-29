1254ded58f3495258a20b25959503a8cd78941f0
drammos <jrammos@outlook.com.gr>
2024-09-29 10:21:35 +0300

    Create JobApplication Repository

---
+++ WebAppBackEnd/MyLink.Data/Repository/IRepository/IJobApplicationsRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyLink.Models;
+
+namespace MyLink.Data.Repository.IRepository;
+
+public interface IJobApplicationsRepository : IRepositoryBase<JobApplication>
+{
+    
+}
---
+++ WebAppBackEnd/MyLink.Data/Repository/JobApplicationsRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyLink.Data.Access;
+using MyLink.Data.Repository.IRepository;
+using MyLink.Models;
+
+namespace MyLink.Data.Repository;
+
+public class JobApplicationsRepository : RepositoryBase<JobApplication>, IJobApplicationsRepository
+{
+    private ApplicationDbContext _context;
+
+    public JobApplicationsRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
