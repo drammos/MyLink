452ba0e1a797a321edee82236db644137a8295a6
drammos <jrammos@outlook.com.gr>
2024-09-29 03:56:22 +0300

    Create a new Dto and table for db

---
+++ WebAppBackEnd/MyLink.Models/ViewedJobs.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLink.Models
+{
+    public class ViewedJobs
+    {
+        [Required] 
+        public int Id { get; set; }
+
+        [Required] 
+        public string? Username { get; set; }
+
+        [Required] 
+        public int JobId { get; set; }
+        [ForeignKey("JobId")] 
+        public Job Job { get; set; } = null!;
+    }
+}
