a885c81b80578d64b25e3b4084d5f1b3434ec53d
drammos <jrammos@outlook.com.gr>
2024-09-29 13:56:23 +0300

    Update with new functions

--- WebAppBackEnd/MyLink.Services/Pagination/PagedList.cs
+++ WebAppBackEnd/MyLink.Services/Pagination/PagedList.cs
@@ -19,9 +19,43 @@ namespace MyLink.Services.Pagination
 
         public static async Task<PagedList<T>> ToPagedList(IQueryable<T> query, int pageNumber, int pageSize)
         {
-            var count = await query.CountAsync();
-            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            var count = await GetCountAsync(query);
+            var items = await GetPageAsync(query, pageNumber, pageSize);
             return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+        
+        public static PagedList<T> ToPagedListInMemory(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = GetCount(query);
+            var items = GetPage(query, pageNumber, pageSize);
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+        private static async Task<int> GetCountAsync(IQueryable<T> query)
+        {
+            if (query is IAsyncEnumerable<T>)
+                return await query.CountAsync();
+            else
+                return query.Count();
+        }
+
+        private static async Task<List<T>> GetPageAsync(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            query = query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+            if (query is IAsyncEnumerable<T>)
+                return await query.ToListAsync();
+            else
+                return query.ToList();
+        }
+
+        private static int GetCount(IQueryable<T> query)
+        {
+            return query.Count();
+        }
+
+        private static List<T> GetPage(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            return query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
         }
     }
 }
